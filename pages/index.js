2aba4f98cd1efa67aa2d77013fef4262a3bc6ffd
YaremenkoM <mariayaremenko13@gmail.com>
2021-08-19 11:38:57 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+        <Head>
+          <title>Openbrush</title>
+          <meta name="keywords" content="openbrush"></meta>
+        </Head>
+        <h1>Welcome</h1>
+    </div>
+  )
+}
