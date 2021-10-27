d75135669a58ed9b7eadc8f8969ea4f16114fbd2
YaremenkoM <YaremenkoM@users.noreply.github.com>
2021-10-27 15:37:32 +0000

    Deploying to gh-pages from @ Supercolony-net/openbrush-website@1496a7dd8986b235b0ea7c2f11866eaf3403a481 🚀

---
+++ _next/static/DcSUbO3b8cwBUY5fX0v-1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-e1372a466a23977a3d67.js","static/css/ec29aeb2eb2f484b18f1.css","static/chunks/pages/index-e11ab645c3618e605b2b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
