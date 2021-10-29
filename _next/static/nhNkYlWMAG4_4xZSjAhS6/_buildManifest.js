c22d0efb15ba195b5cb1d2e27c91a3aed4bda402
YaremenkoM <43481611+YaremenkoM@users.noreply.github.com>
2021-10-29 19:56:53 +0000

    Deploying to gh-pages from @ Supercolony-net/openbrush-website@3618377473a1aec65ccd374a1bbde64f050d5b9f 🚀

---
+++ _next/static/nhNkYlWMAG4_4xZSjAhS6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-e1372a466a23977a3d67.js","static/css/fbd5dba92af104530d9c.css","static/chunks/pages/index-ba1cdee134f2bcabde9a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/z9EsOn38PU1_463XqS2op/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-e1372a466a23977a3d67.js","static/css/79ec169453e59cd2c916.css","static/chunks/pages/index-ef4c052c28386582c186.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
