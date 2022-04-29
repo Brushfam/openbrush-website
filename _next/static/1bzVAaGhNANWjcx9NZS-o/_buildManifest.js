42e65ebbd5e63646b03c5c797f1ec5e270911d43
alex-seleznov <alex.seleznov@gmail.com>
2022-04-29 08:57:56 +0000

    Deploying to gh-pages from @ Supercolony-net/openbrush-website@5f2000cd3fa38c2a02c99802cc20603f0516f14d 🚀

---
+++ _next/static/1bzVAaGhNANWjcx9NZS-o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-e1372a466a23977a3d67.js","static/css/5ca4c52f98ccc323a587.css","static/chunks/pages/index-a1d36b3f5b391c5a7ce8.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Of0fciwaeEEzY2xXKiKqC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-e1372a466a23977a3d67.js","static/css/bbb6df0e7c49be189beb.css","static/chunks/pages/index-c5f3e6544f22331f731f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
