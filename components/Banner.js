135e5feffdeee17df2a4a32e139f56beb420c915
alex-seleznov <alex.seleznov@gmail.com>
2022-04-29 14:14:42 +0300

    ++

--- components/Banner.js
+++ components/Banner.js
@@ -14,43 +14,37 @@ export default function Banner({ title, illustration }) {
 
       <div className={banner.bannerInnerContentHolder}>
 
-        <div>
-          <div>
+        <div className={banner.headlineBlock}>
+          <h1>{title}</h1>
+        </div>
 
-            <div className={banner.headlineBlock}>
-              <h1>{title}</h1>
-            </div>
+        <div className={banner.illustrationConstructor}>
 
-            <div className={banner.illustrationConstructor}>
-
-              <div className={banner.illustrationContainer}>
-                  <img src='/img/ink.svg' alt='ink' />
-                  <span className={banner.ink}>ink!</span>
-              </div>
-
-              <img src='/img/plus.svg' alt='plus' />
-
-              <div className={banner.illustrationContainer}>
-                  <img className={banner.brushImg} src='/img/brush.svg' alt='brush' />
-                  <span className={banner.openbrush}>OpenBrush</span>
-              </div>
-
-              <img src='/img/equals.svg' alt='equals' />
-
-              <div className={banner.illustrationContainer}>
-                <img src='/img/contract.svg' alt='ink' />
-                <span className={banner.contracts}>Smart Contracts</span>
-              </div>
-
-            </div>
-
-            <div className={banner.ctaContainer} >
-              <Link href='/#wizard' >
-                <a onClick={handleClick}>TRY IT OUT</a>
-              </Link>
-            </div>
-
+          <div className={banner.illustrationContainer}>
+              <img src='/img/ink.svg' alt='ink' />
+              <span className={banner.ink}>ink!</span>
           </div>
+
+          <img src='/img/plus.svg' alt='plus' />
+
+          <div className={banner.illustrationContainer}>
+              <img className={banner.brushImg} src='/img/brush.svg' alt='brush' />
+              <span className={banner.openbrush}>OpenBrush</span>
+          </div>
+
+          <img src='/img/equals.svg' alt='equals' />
+
+          <div className={banner.illustrationContainer}>
+            <img src='/img/contract.svg' alt='ink' />
+            <span className={banner.contracts}>Smart Contracts</span>
+          </div>
+
+        </div>
+
+        <div className={banner.ctaContainer} >
+          <Link href='/#wizard' >
+            <a onClick={handleClick}>TRY IT OUT</a>
+          </Link>
         </div>
 
       </div>
