5a3e39c353345bb377ab423656118c19f0e2d996
alex-seleznov <alex.seleznov@gmail.com>
2022-04-29 11:47:42 +0300

    + 'created by SUPER:COLONY'

--- components/Banner.js
+++ components/Banner.js
@@ -1,65 +1,73 @@
 import banner from './../styles/Banner.module.scss'
 import Link from "next/link";
-import {useEffect, useState} from "react";
 
-const Banner = ({ title, illustration }) => {
-    const [anchorTarget, setAnchorTarget] = useState(null);
-    useEffect(() => {
-        setAnchorTarget(document.getElementById('wizard'));
-    }, []);
 
+export default function Banner({ title, illustration }) {
     const handleClick = event => {
         event.preventDefault();
-        anchorTarget.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        document.getElementById('wizard')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
     };
 
 
     return (
     <div className={banner.bannerContainer}>
+
       <div className={banner.bannerInnerContentHolder}>
+
         <div>
           <div>
+
             <div className={banner.headlineBlock}>
               <h1>{title}</h1>
             </div>
+
             <div className={banner.illustrationConstructor}>
-                  <div className={banner.illustrationContainer}>
-                      <img src='/img/ink.svg' alt='ink' />
-                      <span className={banner.ink}>ink!</span>
-                  </div>
 
-                  <img src='/img/plus.svg' alt='plus' />
-
-                  <div className={banner.illustrationContainer}>
-                      <img className={banner.brushImg} src='/img/brush.svg' alt='brush' />
-                      <span className={banner.openbrush}>OpenBrush</span>
-                  </div>
-
-                  <img src='/img/equals.svg' alt='equals' />
-
-                  <div className={banner.illustrationContainer}>
-                    <img src='/img/contract.svg' alt='ink' />
-                    <span className={banner.contracts}>Smart Contracts</span>
-                  </div>
+              <div className={banner.illustrationContainer}>
+                  <img src='/img/ink.svg' alt='ink' />
+                  <span className={banner.ink}>ink!</span>
               </div>
+
+              <img src='/img/plus.svg' alt='plus' />
+
+              <div className={banner.illustrationContainer}>
+                  <img className={banner.brushImg} src='/img/brush.svg' alt='brush' />
+                  <span className={banner.openbrush}>OpenBrush</span>
+              </div>
+
+              <img src='/img/equals.svg' alt='equals' />
+
+              <div className={banner.illustrationContainer}>
+                <img src='/img/contract.svg' alt='ink' />
+                <span className={banner.contracts}>Smart Contracts</span>
+              </div>
+
+            </div>
+
+            <div className={banner.ctaContainer} >
+              <Link href='/#wizard' >
+                <a onClick={handleClick}>TRY IT OUT</a>
+              </Link>
+            </div>
+
           </div>
         </div>
+
       </div>
 
-        <div className={banner.ctaContainer} >
-            <Link href='/#wizard' >
-                <a onClick={handleClick}>TRY IT OUT</a>
-            </Link>
-        </div>
+      <p className={banner.createdBy}>
+        created by<a href="https://www.supercolony.net" target='_blank'>
+          <img src="/img/supercolony.svg" className={banner.supercolony} />
+        </a>
+      </p>
 
       <div className={banner.bannerContainerDecorWrapper}>
-        <div className={banner.leftBottom}></div>
+        <div className={banner.leftBottom} />
         <div className={banner.rightBottom}>
-          <div></div>
+          <div />
         </div>
       </div>
+
     </div>
   )
 }
-
-export default Banner
