90f0ed68b2262740db454157f99fda84670ea1fe
alex-seleznov <alex.seleznov@gmail.com>
2022-04-29 08:49:30 +0000

    Deploying to gh-pages from @ Supercolony-net/openbrush-website@5f5c28b81c37dc9f979a5f47646dda2916ac431e 🚀

--- _next/static/OZFbuERgGAi82WcTPeJgK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-e1372a466a23977a3d67.js","static/css/d7fa7022cf1a1bd1db01.css","static/chunks/pages/index-8353225f250cf9a10a08.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
