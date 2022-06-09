3decc9aed75adb833e25f8f98e19b246ec050337
xgreenx <xgreenx@users.noreply.github.com>
2022-06-09 12:09:57 +0000

    Deploying to gh-pages from @ Supercolony-net/openbrush-website@a2ce7457f6002b4f313037b90f26198677a315c5 🚀

---
+++ _next/static/EL8oz3s4o7mFo5yJ4z9pn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/879-13be451d2d129ba100e0.js","static/css/94f020d26b13773ae981.css","static/chunks/pages/index-6a85b804aa8d631556ab.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
