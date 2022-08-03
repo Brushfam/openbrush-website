5f72a854d612ffb53d0c173f0fdf0964ea778629
Artemka374 <Artemka374@users.noreply.github.com>
2022-08-03 14:26:45 +0000

    Deploying to gh-pages from @ Supercolony-net/openbrush-website@b0b63e106a9f63eac1fa598d93abb04f04ef5110 🚀

--- _next/static/JukwmJFxUfBnhAQj9tLdl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/879-13be451d2d129ba100e0.js","static/css/94f020d26b13773ae981.css","static/chunks/pages/index-68c17fb85bb3bb705656.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
