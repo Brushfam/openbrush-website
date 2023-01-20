bdc1e27e1443652033dc392cca3b3a91818f8157
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2023-01-20 11:29:32 +0200

    structured data

---
+++ components/StructuredData.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function StructuredData({ data }) {
+    return (
+        <script
+            key="structured-data"
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+        />
+    );
+}
