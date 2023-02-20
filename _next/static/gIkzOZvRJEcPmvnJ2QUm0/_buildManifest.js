a13a48b2de88256521c1d1ff02b6360a01bbf198
coreggon11 <43150707+coreggon11@users.noreply.github.com>
2023-02-20 16:35:08 +0000

    Deploying to gh-pages from @ 727-Ventures/openbrush-website@ab7fb2c0e883434f3abd24a7d3ba4464cf309b82 🚀

--- _next/static/gIkzOZvRJEcPmvnJ2QUm0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/557-9b3e5034d0f5ff52.js","static/css/b6573d74baa2b1c2.css","static/chunks/pages/index-f3bbe85cba685f6b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
