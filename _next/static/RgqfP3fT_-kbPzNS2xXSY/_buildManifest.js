d89bf8502c99099b6e37bebde8d37f0c6f9d204a
o-tsaruk <90980937+o-tsaruk@users.noreply.github.com>
2023-04-12 15:26:36 +0000

    Deploying to gh-pages from @ Brushfam/openbrush-website@10c434b7db05a8209f657a482f5c9d36beb26c18 🚀

---
+++ _next/static/RgqfP3fT_-kbPzNS2xXSY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/557-9b3e5034d0f5ff52.js","static/css/b6573d74baa2b1c2.css","static/chunks/pages/index-f308e8769fadfb73.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
